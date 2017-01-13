1f60dc1e0abc0bb4ee72b18782a202cfb756ca94
Mika Lindell <mika.lindell82@gmail.com>
2017-01-13 18:30:57 +0200

    Service to get product data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+// Custom
 import { AppComponent } from './app.component';
+import { ProductService }         from '../product/product.service';
 
 
 // Modules and components made available for the app
@@ -11,6 +14,10 @@ import { AppComponent } from './app.component';
 
   declarations: [
     AppComponent
+  ],
+
+  providers: [
+    ProductService
   ],
 
   bootstrap: [ AppComponent ] // Use this component to start the app
