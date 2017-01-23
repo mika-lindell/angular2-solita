c1957f9ca47a0301c962cb7bf67f330d23ae359c
Mika Lindell <mika.lindell82@gmail.com>
2017-01-23 20:04:42 +0200

    Added small delay for loading screen so it doesnt flicker when app loads fast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ import '../../public/css/styles.css'; // Import styles common for all components
   animations: [
     trigger('fadeIn', [
       transition('void => *', [
-        animate(300, keyframes([
+        animate(400, keyframes([
           style({opacity: 0, offset: 0}),
           style({opacity: 1, offset: 1.0})
         ]))
